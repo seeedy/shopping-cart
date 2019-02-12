caa77a5bb0ef7ec28a8057bd32bfa27cdadbaef7
Christian Siedentop <40824622+seeedy@users.noreply.github.com>
2019-02-12 16:25:39 +0100

    deploying production build to heroku

---
+++ build/precache-manifest.00b532a9f389a762c13cd86e6a1800c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "130b5970ec0c26a1da1d",
+    "url": "/static/js/main.130b5970.chunk.js"
+  },
+  {
+    "revision": "f3b6df0a3a11a9e3dbe9",
+    "url": "/static/js/1.f3b6df0a.chunk.js"
+  },
+  {
+    "revision": "130b5970ec0c26a1da1d",
+    "url": "/static/css/main.0d5186df.chunk.css"
+  },
+  {
+    "revision": "fbea6e90d3e0ea82d276a6df407a147d",
+    "url": "/index.html"
+  }
+];
